5606c11c93ee89472aaad299d2ab05010de5d42f
MateusSpengler <spenglerback86@gmail.com>
2019-08-20 10:52:53 -0300

    BusON 1.0

---
+++ app/src/main/java/com/buson/loginfirebase/Ground.java
@@ -0,0 +1,16 @@
+package com.buson.loginfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ground extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ground);
+
+        setTitle("BusON");
+    }
+}
